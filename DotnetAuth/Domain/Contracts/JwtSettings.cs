e940d4d12a6842bdee459ae2581955a723c4f9f7
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2025-01-09 22:58:15 +0400

    Add JwtSettings

---
+++ DotnetAuth/Domain/Contracts/JwtSettings.cs
@@ -0,0 +1,12 @@
+namespace DotnetAuth.Domain.Contracts;
+
+public class JwtSettings //ეს კლასი შეიცავს JWT(JSON Web Token)-ის კონფიგურაციის პარამეტრებს, რომლებიც საჭიროა ავტორიზაციისა და უსაფრთხოების დასამყარებლად.
+{
+    public string? Key { get; set; }
+    
+    public string ValidIssuer { get; set; } //ტოკენი გენერირებულია მაგ:"https://yourdomain.com"
+    
+    public string ValidAudience { get; set; } // რომ ტოკენი სწორადაა მიმართული შესაბამისი აუდიტორიისკენ მაგ:"https://yourapi.com"
+
+    public double Expires { get; set; } // Expires აღნიშნავს ტოკენის ვადის გასვლის ხანგრძლივობას, ჩვეულებრივ წამებში. 36000second = 1hour
+}
