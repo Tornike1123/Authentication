784761de9e85a24fefc0b3f6052e3c403a72c68e
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2025-01-09 22:45:34 +0400

    Add ErrorResponse

--- DotnetAuth/Domain/Contracts/ErrorResponse.cs
+++ DotnetAuth/Domain/Contracts/ErrorResponse.cs
@@ -1,6 +1,10 @@
 namespace DotnetAuth.Domain.Contracts;
 
-public class ErrorResponse
+public class ErrorResponse //API-ს შეცდომების მართვა: როდესაც API აბრუნებს შეცდომას
 {
+    public string Title { get; set; } //შეცდომის ტიპი (Title)
     
+    public string Message { get; set; } //შესაბამისი შეტყობინება (Message)
+
+    public int StatusCode { get; set; } //HTTP სტატუს კოდი (StatusCode)
 }
