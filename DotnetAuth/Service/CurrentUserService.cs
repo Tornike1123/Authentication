3a1afa3ed4cf0079847a6678b8dd8216bc3f72d3
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2025-01-12 00:50:07 +0400

    ICurrentService and CurrentService

---
+++ DotnetAuth/Service/CurrentUserService.cs
@@ -0,0 +1,32 @@
+using System.Security.Claims;
+
+namespace DotnetAuth.Service;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public CurrentUserService(){}
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    public string? GetUserId()
+    {
+        var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier); //ClaimTypes.NameIdentifier: უნიკალური იდენტიფიკატორი (მომხმარებლის Id).
+        return userId;
+    }
+}
+
+// IHttpContextAccessor არის ASP.NET Core-ის სერვისი, რომელიც საშუალებას გაძლევთ მიიღოთ მიმდინარე HTTP მოთხოვნის კონტექსტი (HttpContext).
+
+//მეთოდი იღებს HTTP კონტექსტში არსებული მომხმარებლის ინფორმაციას.
+// მომხმარებლის იდენტიფიკაცია ხდება Claims საშუალებით:
+// ClaimTypes.NameIdentifier ჩვეულებრივ გამოიყენება მომხმარებლის უნიკალური Id-ის შესანახად.
+// FindFirstValue აბრუნებს კონკრეტული Claim-ის მნიშვნელობას.
+
+// გამოყენების მაგალითი: თუ მომხმარებელი ავტორიზებულია, GetUserId() დააბრუნებს მომხმარებლის უნიკალურ Id-ს,
+// რომელიც ჩვეულებრივ შეესაბამება მონაცემთა ბაზაში მომხმარებლის ჩანაწერის პირველადი კლავიშს.
+
+//Step 8
---
+++ DotnetAuth/Service/ICurrentUserService.cs
@@ -0,0 +1,12 @@
+namespace DotnetAuth.Service;
+
+public interface ICurrentUserService
+{
+    public string? GetUserId(); //GetUserId(): უნდა დააბრუნოს მიმდინარე მომხმარებლის Id.
+}
+
+
+
+
+
+//Step 7
