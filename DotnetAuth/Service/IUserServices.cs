92cc183dd618c7432b49ed662896f19b33215998
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2025-01-18 22:23:08 +0400

    Add IUserServices

--- DotnetAuth/Service/IUserServices.cs
+++ DotnetAuth/Service/IUserServices.cs
@@ -4,14 +4,21 @@ namespace DotnetAuth.Service;
 
 public interface IUserServices
 {
-    Task<UserResponse> RegisterAsync(UserRegisterRequest request);
-    Task<CurrentUserResponse> GetCurrentUserAsync();
-    Task<UserResponse> GetByIdAsync(Guid id);
-    Task<UserResponse> UpdateAsync(Guid id, UpdateUserRequest request);
-    Task DeleteAsync(Guid id);
-    Task<RevokeRefreshTokenResponse> RevokeRefreshToken(RefreshTokenRequest refreshTokenRemoveRequest);
-    Task<CurrentUserResponse> RefreshTokenAsync(RefreshTokenRequest request);
-    Task<UserResponse> LoginAsync(UserLoginRequest request);
+    Task<UserResponse> RegisterAsync(UserRegisterRequest request);//(UserRequestAndRespons-დან)
+    Task<CurrentUserResponse> GetCurrentUserAsync();//(UserRequestAndRespons-დან)
+    Task<UserResponse> GetByIdAsync(Guid id);//(UserRequestAndRespons-დან)
+    Task<UserResponse> UpdateAsync(Guid id, UpdateUserRequest request);//(UserRequestAndRespons-დან)
+    Task DeleteAsync(Guid id); //მომხმარებლის წაშლა სისტემიდან
+    Task<RevokeRefreshTokenResponse> RevokeRefreshToken(RefreshTokenRequest refreshTokenRemoveRequest);//(UserRequestAndRespons-დან)
+    Task<CurrentUserResponse> RefreshTokenAsync(RefreshTokenRequest request);//(UserRequestAndRespons-დან)
+    Task<UserResponse> LoginAsync(UserLoginRequest request);//(UserRequestAndRespons-დან)
 }
+
+//IUserServices წარმოადგენს მომხმარებლის მართვის სისტემის მთავარ ინტერფეისს(იღებს UserRequestAndRespons-დან)
+// რეგისტრაცია
+// ავტორიზაცია და ტოკენების მართვა
+// მომხმარებლის დეტალების მიღება და განახლება
+// წაშლა და ტოკენების გაუქმება
+
 
 //Step 11
